48b27b255df68ae6545697cb3c76357755a3f23c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-31 13:27:07 +0600

    react with tailwind css. install tailwind css in react project done on 07/31/2025 at 01:27 pm

---
+++ 07_react_with_tailwind_css/react_with_tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-slate-900 flex items-center justify-center">
+        <h1 className="text-5xl bg-emerald-700 p-6 rounded-md text-white">Tailwind Installing Successful</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
